d5f934b45610e5ee58d9b3cfc90a174e96a2701a
Poetic_Programmer <michaelgeorgedunleavy@gmail.com>
2021-07-20 18:45:49 +0100

    added method for getting string between two keys. refactored accordingly.

--- Scene Parser/MainWindow.xaml.cs
+++ Scene Parser/MainWindow.xaml.cs
@@ -21,14 +21,14 @@ namespace Scene_Parser
     public partial class MainWindow : Window
     {
         TextReader reader;
-        List<string> names;
+        List<DialogueSequence> sequence;
         string text = "";
         public MainWindow()
         {
             InitializeComponent();
 
             reader = new TextReader();
-            names = new List<string>();
+            sequence = new List<DialogueSequence>();
         }
 
         private void LoadTextButtonClick(object sender, EventArgs args)
@@ -43,32 +43,49 @@ namespace Scene_Parser
             var sequences = text.Split(seperators, StringSplitOptions.RemoveEmptyEntries);
             foreach(string s in sequences)
             {
-                ParseSequence(s);
+                sequence.Add(ParseSequence(s));
             }
-            NamesTextBox.Text = BuildStringList(names);
+
+            ParsedTextBox.Text = GetAllDialogueForPrinting();
+            NamesTextBox.Text = GetAllNamesForPrinting();
         }
 
-        private void ParseSequence(string sequence)
-        {
-            int index = sequence.IndexOf("<character>") + "<character>".Length;
-            int endIndex = sequence.IndexOf("</character>");
-            int length = endIndex - index;
-            string name = sequence.Substring(index, length);
-
-            names.Add(name);
-        }
-
-        private string BuildStringList(List<string> lines)
+        private string GetAllNamesForPrinting()
         {
             StringBuilder builder = new StringBuilder();
-            foreach (string s in lines)
+            foreach(DialogueSequence dialogue in sequence)
             {
-                var edit = s.Trim();
-                builder.Append(edit);
+                builder.Append(dialogue.SpeakerName);
                 builder.Append("\n");
-                Console.WriteLine(string.Format("Length of line: {0}", edit.Length));
             }
             return builder.ToString();
+        }
+
+        private string GetAllDialogueForPrinting()
+        {
+            StringBuilder builder = new StringBuilder();
+            foreach (DialogueSequence dialogue in sequence)
+            {
+                builder.Append(dialogue.Dialogue);
+                builder.Append("\n");
+            }
+            return builder.ToString();
+        }
+
+        private DialogueSequence ParseSequence(string text)
+        {
+            string name = GetTextBetween(text, "<character>", "</character>");
+            string dialogue = GetTextBetween(text, "<dialogue>", "</dialogue>").Trim();
+
+            return new DialogueSequence(name, dialogue);
+        }
+
+        private string GetTextBetween(string text, string startKey, string endKey)
+        {
+            int startIndex = text.IndexOf(startKey) + startKey.Length;
+            int endIndex = text.IndexOf(endKey);
+            int length = endIndex - startIndex;
+            return text.Substring(startIndex, length);
         }
     }
 }
---
+++ Scene Parser/Scripts/DialogueSequence.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scene_Parser
+{
+    class DialogueSequence
+    {
+        public readonly string SpeakerName;
+        public readonly string Dialogue;
+
+        public DialogueSequence(string speakerName, string dialogue)
+        {
+            SpeakerName = speakerName;
+            Dialogue = dialogue;
+        }
+    }
+}
